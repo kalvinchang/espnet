ba1b2e1d95732bd40a2cbc273eace6571f814f3d
Kalvin Chang <kalvin1204@gmail.com>
2024-09-28 20:12:30 -0400

    feat: dump XEUS features

---
+++ egs2/ipapack/asr1/local/dump_xeus_feats.py
@@ -0,0 +1,116 @@
+from pyscripts.feats.ssl_feature_utils import BaseFeatureReader, dump_feature
+
+import torch
+
+
+# adapted from https://github.com/simpleoier/ESPnet_SSL_ASR_tutorial_misc/blob/main/dump_feats.py
+
+
+class XEUSFeatureReader(BaseFeatureReader):
+    def __init__(self, checkpoint_path, layer=-1):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        assert -1 <= layer <= 18
+        self.layer = layer
+    
+        xeus_model, xeus_train_args = SSLTask.build_model_from_file(
+            config_file=None,
+            model_file=checkpoint_path,
+            device=self.device
+        )
+        self.model = xeus_model
+
+    def get_feats(
+        self, data: torch.Tensor, data_lens: torch.Tensor, ref_len: Optional[int] = None
+    ):
+        # we recommend use_mask=True during fine-tuning
+        # take the output of the last layer -> batch_size x seq_len x hdim
+        with torch.no_grad():
+            x, x_lens = self.preprocess_data(data, data_lens)
+            wavs = wavs.to(self.device)
+            # TODO: allow linear combo of layers??
+            feats = model.encode(wavs, data_lens, use_mask=False, use_final_output=False)[0][self.layer]
+            # ex: [1, 1097, 1024] for 20 s file
+
+            # TODO: figure out feature lengths
+            feats_lens = 
+        return feats.cpu(), feats_lens.cpu()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--feature_type", type=str, default="mfcc", choices=["mfcc", "hubert", "s3prl"]
+    )
+    parser.add_argument("--hubert-model-url", type=str, default=None)
+    parser.add_argument("--hubert-model-path", type=str, default=None)
+    parser.add_argument("--s3prl-upstream-name", type=str, default=None)
+    parser.add_argument("--layer", type=int, default=None)
+    parser.add_argument("--sample_rate", type=int, default=16000)
+    parser.add_argument("--max_chunk", type=int, default=1600000)
+    parser.add_argument("--seed", default=0, type=int)
+    parser.add_argument(
+        "--hubert_type",
+        type=str,
+        default="espnet",
+        choices=["espnet", "fairseq"],
+        help="Whether the HuBERT encoder implementation is based on espnet or fairseq.",
+    )
+    parser.add_argument(
+        "--in_filetype",
+        type=str,
+        default="sound",
+        choices=["mat", "hdf5", "sound.hdf5", "sound"],
+        help="Specify the file format for the rspecifier. "
+        '"mat" is the matrix format in kaldi',
+    )
+    parser.add_argument(
+        "--out_filetype",
+        type=str,
+        default="npy",
+        choices=["npy", "mat", "hdf5"],
+        help="Specify the file format for the wspecifier. "
+        '"npy" is the matrix format in kaldi',
+    )
+    parser.add_argument(
+        "--write_num_frames", type=str, help="Specify wspecifer for utt2num_frames"
+    )
+    parser.add_argument(
+        "rspecifier", type=str, help="Read specifier for feats. e.g. ark:some.ark"
+    )
+    parser.add_argument(
+        "wspecifier", type=str, help="Write specifier. e.g. ark:some.ark"
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    exit()
+
+    checkpoint_path='/ocean/projects/cis210027p/kchang1/XEUS/model/xeus_checkpoint.pth'
+    reader = XEUSFeatureReader(
+                checkpoint_path=checkpoint_path,
+                layer=-1
+            )
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s",
+    )
+    logger = logging.getLogger("sklearn_kmeans")
+
+    parser = get_parser()
+    args = parser.parse_args()
+    logging.info(str(args))
+
+    np.random.seed(args.seed)
+    logging.info("Loading Features")
+
+    dump_feature(
+        reader,
+        in_filetype=args.in_filetype,
+        rspecifier=args.rspecifier,
+        out_filetype=args.out_filetype,
+        wspecifier=args.wspecifier,
+        write_num_frames=args.write_num_frames,
+    )
