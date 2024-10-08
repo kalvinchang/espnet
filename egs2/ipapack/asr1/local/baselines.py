cd1d3a6f26101a97cd805ef0f96e26128c5482f1
Kalvin Chang <kalvin1204@gmail.com>
2024-10-07 23:33:57 -0400

    feat: train loop for baselines

--- egs2/ipapack/asr1/local/baselines.py
+++ egs2/ipapack/asr1/local/baselines.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from local.dataset import IPAPack
 from espnet2.asr.ctc import CTC
 from espnet2.asr.encoder.linear_encoder import LinearEncoder
@@ -8,18 +10,18 @@ from espnet2.tasks.ssl import SSLTask
 import torch.nn
 from s3prl.nn import Featurizer
 from panphon import FeatureTable
+from tqdm import tqdm
 
 
 # TODO: if time, could make this into a Frontend (multilayer_feature)
 class XEUSEncoder(nn.Module):
     # meant to be finetuned
-    def __init__(self, checkpoint_path):
+    def __init__(self, checkpoint_path, device):
         super().__init__()
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         xeus_model, xeus_train_args = SSLTask.build_model_from_file(
             config_file=None,
             model_file=checkpoint_path,
-            device=self.device
+            device=device
         )
         self.model = xeus_model
 
@@ -28,7 +30,7 @@ class XEUSEncoder(nn.Module):
         speech: torch.Tensor,
         speech_lengths: torch.Tensor
     ):
-        wavs = speech.to(self.device)
+        wavs = speech
         # source: https://www.wavlab.org/activities/2024/xeus/
         # (xs_pad, intermediate_outs), olens, None
         # we recommend use_mask=True during fine-tuning
@@ -63,35 +65,131 @@ class FinetuneXEUSPhonemeCTC(nn.Module):
         return self.encoder(self.weighted_sum(self.xeus(speech, speech_lengths)))
 
 
-def train(artic_feats):
-    # takes in XEUS features and adds single CTC head
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Phoneme recognition baselines"
+    )
+    parser.add_argument(
+        "--articulatory_losses",
+        type=bool,
+        default=False,
+        required=True
+    )
+    parser.add_argument(
+        "--train_dir",
+        type=Path,
+        default=Path("data/train"),
+        required=True
+    )
+    parser.add_argument(
+        "--dev_dir",
+        type=Path,
+        default=Path("data/dev"),
+        required=True
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=50,
+        required=True
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.001,
+        required=True
+    )
+    return parser
+
+
+def dev_step(model, optimizer, dev_loader, phoneme_ctc):
+    model.eval()
+    total_loss = 0
+    for i, (utt_ids, batch) in tqdm(enumerate(dev_loader)):
+        speech_lens = batch["speech_lengths"]
+        phonemes, phoneme_len = batch["text"], batch["text_lengths"]
+
+        logits = model(**batch)
+        loss = phoneme_ctc(logits, phonemes, speech_lens, phoneme_len)
+        # we don't care about articulatory loss as a metric b/c it's just an auxiliary loss
+        total_loss += loss.item()
+        num_updates += 1
+
+    return total_loss / num_updates
+
+
+def train_step(model, optimizer, train_loader, phoneme_ctc, articulatory_ctc=None):
+    model.train()
+    total_loss, num_updates = 0, 0
+    for i, (utt_ids, batch) in tqdm(enumerate(train_loader)):
+        optimizer.zero_grad()
+
+        # TODO: do the .to(device) here?
+
+        # TODO: may need to do this? https://espnet.github.io/espnet/guide/espnet2/train/common_collate_fn.html
+        # utt_ids: list of utt id's
+        # batch: {"speech": ..., "speech_lengths": ..., "text": ..., "text_lengths": ...}
+        logits = model(**batch)
+
+        speech_lens = batch["speech_lengths"]
+        phonemes, phoneme_len = batch["text"], batch["text_lengths"]
+        # TODO: should the model return speech_lens?
+        # ESPnet does the log_softmax for you
+        loss = phoneme_ctc(logits, phonemes, speech_lens, phoneme_len)
+        if articulatory_ctc:
+            for aux_loss in articulatory_ctc:
+                loss += aux_loss(logits)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        num_updates += 1
+
+        if i % 1000 === 999:
+            # TODO: plot the loss
+            pass
+
+    return total_loss / num_updates
+
+
+def train(model, optimizer, train_loader, dev_loader, phoneme_ctc, articulatory_ctc=None, artic_feats, epochs, device):
+    for epoch in tqdm(range(epochs)):
+        train_loss = train_step(model, train_loader, phoneme_ctc, articulatory_ctc, device)
+        # TODO: save the best model(?)
+        # TODO: model averaging?
+        # TODO: learning rate scheduler
+        dev_loss = dev_step(model, dev_loader, phoneme_ctc, device)
+
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    checkpoint_path = '/ocean/projects/cis210027p/kchang1/XEUS/model/xeus_checkpoint.pth'
+    model = FinetuneXEUSPhonemeCTC(checkpoint_path, device)
+
+    parser = get_parser()
+    args = parser.parse_args()
+    print(args)
+
+    train_dset = IPAPack(scp_file=f"{args.train_dir}/feats.scp", text_file=f"{args.train_dir}/text")
+    # TODO: is the collate_fn necessary if it's already an ESPnet dataset?
+    # TODO: batch_bins ?
+    train_loader = DataLoader(train_dset, batch_size=4, collate_fn=common_collate_fn)
+    dev_dset = IPAPack(scp_file=f"{args.dev_dir}/feats.scp", text_file=f"{args.dev_dir}/text")
+    dev_loader = DataLoader(dev_dset, batch_size=4, collate_fn=common_collate_fn)
+
     # TODO: how does ESPnet infer the vocab_size? from a token_list ?
     phoneme_ctc = CTC(odim=vocab_size, encoder_output_size=xeus_model_dim)
     phoneme_ctc_loss = ctc.loss_fn
 
-    articulatory_ctc_losses = [CTC(odim=3, encoder_output_size=xeus_model_dim) for _ in artic_feats]
-    pass
-
-
-if __name__ == "__main__":
-    checkpoint_path = '/ocean/projects/cis210027p/kchang1/XEUS/model/xeus_checkpoint.pth'
-    model = FinetuneXEUSPhonemeCTC(checkpoint_path)
-    
-    # TODO: argparse
-    train_dset = IPAPack(scp_file="data/train/feats.scp", text_file="data/train/text")
-    # TODO: is the collcate_fn necessary if it's already an ESPnet dataset?
-    # TODO: batch_bins ?
-    train_loader = DataLoader(dataset, batch_size=4, collate_fn=common_collate_fn)
+    articulatory_ctc_losses = None
+    if args.articulatory_losses:
+        articulatory_ctc_losses = [CTC(odim=3, encoder_output_size=xeus_model_dim).loss_fn for _ in artic_feats]
 
     ft = FeatureTable()
     artic_feats = ft.names
-    
-    # TODO: train loop
-    for utt_ids, batch in train_loader:
-        # TODO: may need to do this? https://espnet.github.io/espnet/guide/espnet2/train/common_collate_fn.html
 
-        # utt_ids: list of utt id's
-        # batch: {"speech": ..., "speech_lengths": ..., "text": ..., "text_lengths": ...}
-        pass
-
-    model(**batch)
+    # TODO: take in beta from config
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=args.learning_rate
+    )
+    train(model, optimizer, train_loader, dev_loader, phoneme_ctc_loss, articulatory_ctc_losses, artic_feats, args.epochs, device)
