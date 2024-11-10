1d437b7d33d392751b7501f4c3a5a0737a7873c9
eunjung31 <eyeo2@andrew.cmu.edu>
2024-11-10 18:21:33 -0500

    update baseline scripts: dataloader, exp

--- egs2/ipapack/asr1/local/baselines.py
+++ egs2/ipapack/asr1/local/baselines.py
@@ -1,17 +1,21 @@
+import argparse
 from pathlib import Path
 
-from local.dataset import IPAPack
+from dataset import IPAPack
 from espnet2.asr.ctc import CTC
 from espnet2.asr.encoder.linear_encoder import LinearEncoder
-from espnet2.asr.preencoder.linear import LinearProjection
 from espnet2.train.collate_fn import common_collate_fn
 from espnet2.tasks.ssl import SSLTask
 
-import torch.nn
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from s3prl.nn import Featurizer
 from panphon import FeatureTable
 from tqdm import tqdm
-
+import jiwer
 
 # TODO: if time, could make this into a Frontend (multilayer_feature)
 class XEUSEncoder(nn.Module):
@@ -24,46 +28,71 @@ class XEUSEncoder(nn.Module):
             device=device
         )
         self.model = xeus_model
+        self.model.frontend = None ## skip frontend because we already have features extracted (feats.scp)
+        self.model.preencoder = None ## skip preencoder because we already have features extracted (feats.scp)
+        if not hasattr(self.model, 'hidden_sizes'):
+            self.model.hidden_sizes = [self.model.encoder.output_size]
+        if not hasattr(self.model, 'downsample_rates'):
+            self.model.downsample_rates = [1] ## no downsampling
 
     def forward(
         self,
         speech: torch.Tensor,
         speech_lengths: torch.Tensor
     ):
-        wavs = speech
         # source: https://www.wavlab.org/activities/2024/xeus/
         # (xs_pad, intermediate_outs), olens, None
         # we recommend use_mask=True during fine-tuning
-        # TODO: double check the list format
-        final_feats, feats = self.model.encode(wavs, speech_lengths, use_mask=True, use_final_output=False)[0]
+        # TODO: use_mask=True (EJ)
+        feats = self.model.encode(speech=speech, speech_lengths=speech_lengths, use_mask=False, use_final_output=False)[0]
         # ex: List of [batch, frames, model_dim] tensors
-
-        # based on https://github.com/pytorch/audio/blob/ba696ea3dfec4cbe693bf06a84c75dc196077f5b/src/torchaudio/models/wav2vec2/model.py#L85
-            # just return the length of the original data
-            # the # frames of each item pre-padding
-        return feats, speech_lengths
+        return feats
 
 
 class FinetuneXEUSPhonemeCTC(nn.Module):
-    def __init__(self, checkpoint_path):
+    def __init__(self, checkpoint_path, device, vocab_size):
         super().__init__()
-        self.xeus = XEUSEncoder(checkpoint_path)
+        self.xeus = XEUSEncoder(checkpoint_path, device)
         # TODO: load from file
         self.xeus.model.num_layers = 19
         # TODO: may need to cast model into S3PRLUpstream
         self.weighted_sum = Featurizer(upstream=self.xeus.model)
         # don't downsample to preserve as much temporal resolution as possible
-        self.encoder = LinearEncoder(input_size=1024, output_size=256, dropout_rate=0.1, input_layer="linear", normalize_before: bool = True)
+        self.encoder = LinearEncoder(input_size=1024, output_size=256, dropout_rate=0.1, input_layer="linear", normalize_before = True)
+        self.ctc = CTC(odim=vocab_size + 1, encoder_output_size=256) # +1 for blank token
 
     def forward(
         self,
         speech: torch.Tensor,
         speech_lengths: torch.Tensor,
-        text: torch.Tensor = None,
-        text_lengths: torch.Tensor = None,
     ):
-        return self.encoder(self.weighted_sum(self.xeus(speech, speech_lengths)))
+        feats = self.xeus(speech, speech_lengths)
+        all_lens = [speech_lengths for _ in feats]
+        weighted_sum_feats, hs_len = self.weighted_sum(feats, all_lens)
+        logits = self.encoder(weighted_sum_feats, hs_len)[0]        
+        return logits, hs_len
+    
+    def compute_loss(self, logits, labels, input_lengths, label_lengths):
+        log_probs = logits.permute(1, 0, 2) 
+        phoneme_ctc_loss = self.ctc.loss_fn(log_probs, labels, input_lengths, label_lengths)
+        return phoneme_ctc_loss
+    
+    def decode(self, logits, input_lengths):
+        log_probs = nn.functional.log_softmax(logits, dim=-1)
+        preds = torch.argmax(log_probs, dim=-1).cpu().numpy()  # Shape: [batch_size, max_time]
 
+        sequences = []
+        blank_id = 0
+        for i, pred in enumerate(preds):
+            pred = pred[:input_lengths[i]]
+            seq = []
+            prev_token = None
+            for token in pred:
+                if token != blank_id and token != prev_token:
+                    seq.append(token)
+                prev_token = token
+            sequences.append(seq)
+        return sequences
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -71,9 +100,8 @@ def get_parser():
     )
     parser.add_argument(
         "--articulatory_losses",
-        type=bool,
         default=False,
-        required=True
+        required=False
     )
     parser.add_argument(
         "--train_dir",
@@ -101,95 +129,117 @@ def get_parser():
     )
     return parser
 
+def train_step(model, optimizer, train_loader, articulatory_ctc, device):
+    accumulation_steps = 2
+    model.train()
+    total_loss = 0
+    for i, batch in tqdm(enumerate(train_loader)):
+        optimizer.zero_grad()
+        # batch: {"speech": ..., "speech_lengths": ..., "text": ..., "text_lengths": ...}
+        utt_ids, data = batch
+        speech = data["speech"].to(device)
+        speech_lengths = data["speech_lengths"].to(device)
+        text = data["text"].to(device)
+        text_lengths = data["text_lengths"].to(device)
+        
+        logits, input_lengths = model(speech, speech_lengths)
+        loss = model.compute_loss(logits, text, input_lengths, text_lengths)
 
-def dev_step(model, optimizer, dev_loader, phoneme_ctc):
+        if articulatory_ctc == True:
+            ft = FeatureTable()
+            artic_feats = ft.names
+            articulatory_ctc_losses = [CTC(odim=3, encoder_output_size=256).loss_fn for _ in artic_feats]
+            for aux_loss in articulatory_ctc_losses:
+                loss += aux_loss(logits)
+        else:
+            pass
+        loss = loss / accumulation_steps
+        loss.backward()
+        if (i + 1) % accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            torch.cuda.empty_cache()  # Clear CUDA cache periodically
+
+        total_loss += loss.item() * accumulation_steps  # Reverse the earlier division for accuracy
+    return total_loss / len(train_loader)
+
+def dev_step(model, dev_loader, device):
     model.eval()
     total_loss = 0
-    for i, (utt_ids, batch) in tqdm(enumerate(dev_loader)):
-        speech_lens = batch["speech_lengths"]
-        phonemes, phoneme_len = batch["text"], batch["text_lengths"]
+    total_wer = 0
+    total_samples = 0
+    padding_value = -32768 ## based on common_collate_fn
+    with torch.no_grad():
+        for batch in dev_loader:
+            utt_ids, data = batch
+            speech = data["speech"].to(device)
+            speech_lengths = data["speech_lengths"].to(device)
+            text = data["text"].to(device)
+            text_lengths = data["text_lengths"].to(device)
 
-        logits = model(**batch)
-        loss = phoneme_ctc(logits, phonemes, speech_lens, phoneme_len)
-        # we don't care about articulatory loss as a metric b/c it's just an auxiliary loss
-        total_loss += loss.item()
-        num_updates += 1
+            logits, input_lengths = model(speech, speech_lengths)
+            loss = model.compute_loss(logits, text, input_lengths, text_lengths)
+            total_loss += loss.item()
+            
+            # Decode the logits to get predicted text sequences
+            predicted_sequences = model.decode(logits, input_lengths)
+            
+            # Convert target sequences to text for WER calculation
+            target_sequences = []
+            for idx_seq in text.cpu().numpy():
+                seq = [train_loader.dataset.int_to_char[idx] for idx in idx_seq if idx != 0 and idx != padding_value]
+                target_sequences.append(" ".join(seq))
 
-    return total_loss / num_updates
-
-
-def train_step(model, optimizer, train_loader, phoneme_ctc, articulatory_ctc=None):
-    model.train()
-    total_loss, num_updates = 0, 0
-    for i, (utt_ids, batch) in tqdm(enumerate(train_loader)):
-        optimizer.zero_grad()
-
-        # TODO: do the .to(device) here?
-
-        # TODO: may need to do this? https://espnet.github.io/espnet/guide/espnet2/train/common_collate_fn.html
-        # utt_ids: list of utt id's
-        # batch: {"speech": ..., "speech_lengths": ..., "text": ..., "text_lengths": ...}
-        logits = model(**batch)
-
-        speech_lens = batch["speech_lengths"]
-        phonemes, phoneme_len = batch["text"], batch["text_lengths"]
-        # TODO: should the model return speech_lens?
-        # ESPnet does the log_softmax for you
-        loss = phoneme_ctc(logits, phonemes, speech_lens, phoneme_len)
-        if articulatory_ctc:
-            for aux_loss in articulatory_ctc:
-                loss += aux_loss(logits)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-        num_updates += 1
-
-        if i % 1000 === 999:
-            # TODO: plot the loss
-            pass
-
-    return total_loss / num_updates
-
-
-def train(model, optimizer, train_loader, dev_loader, phoneme_ctc, articulatory_ctc=None, artic_feats, epochs, device):
-    for epoch in tqdm(range(epochs)):
-        train_loss = train_step(model, train_loader, phoneme_ctc, articulatory_ctc, device)
-        # TODO: save the best model(?)
-        # TODO: model averaging?
-        # TODO: learning rate scheduler
-        dev_loss = dev_step(model, dev_loader, phoneme_ctc, device)
-
+            # Calculate WER for each prediction
+            for pred, target in zip(predicted_sequences, target_sequences):
+                pred_text = " ".join([train_loader.dataset.int_to_char[idx] for idx in pred])
+                total_wer += jiwer.wer(target, pred_text)
+                total_samples += 1
+    
+    avg_loss = total_loss / len(dev_loader)
+    avg_wer = total_wer / total_samples
+    return avg_loss, avg_wer
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     checkpoint_path = '/ocean/projects/cis210027p/kchang1/XEUS/model/xeus_checkpoint.pth'
-    model = FinetuneXEUSPhonemeCTC(checkpoint_path, device)
 
     parser = get_parser()
     args = parser.parse_args()
     print(args)
 
-    train_dset = IPAPack(scp_file=f"{args.train_dir}/feats.scp", text_file=f"{args.train_dir}/text")
-    # TODO: is the collate_fn necessary if it's already an ESPnet dataset?
+    train_dset = IPAPack(scp_file=f"{args.train_dir}/feats.scp", text_file=f"{args.train_dir}/text", utt2dur_file="train_utt2dur")
     # TODO: batch_bins ?
-    train_loader = DataLoader(train_dset, batch_size=4, collate_fn=common_collate_fn)
-    dev_dset = IPAPack(scp_file=f"{args.dev_dir}/feats.scp", text_file=f"{args.dev_dir}/text")
-    dev_loader = DataLoader(dev_dset, batch_size=4, collate_fn=common_collate_fn)
+    train_loader = DataLoader(train_dset, batch_size=1, shuffle=True, collate_fn=common_collate_fn, num_workers=4)
+    dev_dset = IPAPack(scp_file=f"{args.dev_dir}/feats.scp", text_file=f"{args.dev_dir}/text", utt2dur_file="dev_utt2dur")
+    dev_loader = DataLoader(dev_dset, batch_size=1, collate_fn=common_collate_fn, num_workers=4)
 
-    # TODO: how does ESPnet infer the vocab_size? from a token_list ?
-    phoneme_ctc = CTC(odim=vocab_size, encoder_output_size=xeus_model_dim)
-    phoneme_ctc_loss = ctc.loss_fn
-
-    articulatory_ctc_losses = None
-    if args.articulatory_losses:
-        articulatory_ctc_losses = [CTC(odim=3, encoder_output_size=xeus_model_dim).loss_fn for _ in artic_feats]
-
-    ft = FeatureTable()
-    artic_feats = ft.names
+    vocab_size = train_dset.get_vocab_size()
+    print(f"Vocabulary size: {vocab_size}")
+    model = FinetuneXEUSPhonemeCTC(checkpoint_path=checkpoint_path, device=device, vocab_size=vocab_size).to(device)
+    writer = SummaryWriter(log_dir="runs/fine_tuning_xeus_phoneme_ctc")
 
     # TODO: take in beta from config
     optimizer = torch.optim.Adam(
         model.parameters(),
-        lr=args.learning_rate
+        lr=args.lr
     )
-    train(model, optimizer, train_loader, dev_loader, phoneme_ctc_loss, articulatory_ctc_losses, artic_feats, args.epochs, device)
+
+    best_dev_wer = float('inf') 
+    for epoch in range(args.epochs):
+        train_loss = train_step(model, optimizer, train_loader, args.articulatory_losses, device)
+        dev_loss, dev_wer = dev_step(model, dev_loader, device)
+
+        writer.add_scalar("Loss/Train", train_loss, epoch)
+        writer.add_scalar("Loss/Validation", dev_loss, epoch)
+        writer.add_scalar("WER/Validation", dev_wer, epoch)
+
+        print(f"Epoch {epoch + 1}, Train Loss: {train_loss:.4f}, Dev Loss: {dev_loss:.4f}, Dev WER: {dev_wer:.4f}")
+
+        # Check if the current model is the best so far
+        if dev_wer < best_dev_wer:
+            best_dev_wer = dev_wer
+            torch.save(model.state_dict(), "best_model.pth")
+            print(f"New best model saved with Dev WER: {best_dev_wer:.4f}")
+
+    writer.close()
--- egs2/ipapack/asr1/local/dataset.py
+++ egs2/ipapack/asr1/local/dataset.py
@@ -4,19 +4,13 @@ from espnet2.train.dataset import ESPnetDataset
 import os
 
 class IPAPack(ESPnetDataset):
-    def __init__(
-        self, 
-        scp_file, 
-        text_file, 
-        utt2dur_file, 
+    def __init__(self, scp_file, text_file, utt2dur_file, 
         base_path="/ocean/projects/cis210027p/kchang1/espnet/egs2/ipapack/asr1", 
-        frame_shift=0.01,  # in seconds
-        max_duration=20.0  # in seconds
+        max_audio_duration=20.0 
     ):
         self.data = {}
         self.base_path = base_path
-        self.frame_shift = frame_shift  # Frame shift in seconds
-        self.max_duration = max_duration  # Maximum allowed duration in seconds
+        self.max_audio_duration = max_audio_duration  # Maximum allowed duration in seconds
 
         # Load feature paths
         self._load_scp(scp_file)
@@ -31,24 +25,19 @@ class IPAPack(ESPnetDataset):
         self.int_to_char = {idx: token for token, idx in self.char_to_int.items()}  # Reverse mapping for decoding
         self.vocab_size = len(unique_tokens)
 
-        # Filter out entries with audio longer than max_duration
-        if utt2dur_file:
-            self._filter_by_duration_utt2dur(utt2dur_file)
-        else:
-            self._filter_by_duration_features()
+        # Filter long audio utterances)
+        self._filter_by_duration_utt2dur(utt2dur_file)
 
         # Final list of utterance IDs
         self.utt_ids = sorted(self.data.keys())
 
     def _load_scp(self, scp_file):
-        """Load feature paths from scp file."""
         with open(scp_file, 'r') as f:
             for line in f:
                 utt_id, path = line.strip().split(' ', 1)
                 self.data[utt_id] = {"feats_path": path}
 
     def _load_text(self, text_file):
-        """Load text data and associate it with utterance IDs."""
         with open(text_file, 'r') as f:
             for line in f:
                 parts = line.strip().split(' ', 1)
@@ -60,11 +49,11 @@ class IPAPack(ESPnetDataset):
                 else:
                     print(f"Warning: Text for {utt_id} not found in filtered features.")
 
-        # Remove entries without text
+        # Remove entries without text (empty texts)
         self.data = {utt_id: entry for utt_id, entry in self.data.items() if "text" in entry}
 
     def compute_vocab_size(self, text_file):
-        """Compute the set of unique tokens (phonemes) from the text file."""
+        """Compute the set of unique phonemes from the text file."""
         with open(text_file, 'r') as f:
             phoneme_set = set()
             for line in f:
@@ -90,31 +79,10 @@ class IPAPack(ESPnetDataset):
             if duration is None:
                 print(f"Warning: Duration for {utt_id} not found in utt2dur. Skipping.")
                 continue
-            if duration <= self.max_duration:
+            if duration <= self.max_audio_duration:
                 filtered_data[utt_id] = entry
             else:
-                print(f"Skipping {utt_id}: duration {duration:.2f}s exceeds {self.max_duration}s")
-        
-        self.data = filtered_data
-
-    def _filter_by_duration_features(self):
-        """Filter utterances based on durations computed from feature files."""
-        filtered_data = {}
-        for utt_id, entry in self.data.items():
-            feat_path = os.path.join(self.base_path, entry['feats_path'])
-
-            try:
-                features = kaldiio.load_mat(feat_path)
-                num_frames = features.shape[0]
-                duration = num_frames * self.frame_shift  # duration in seconds
-                if duration <= self.max_duration:
-                    filtered_data[utt_id] = entry
-                else:
-                    print(f"Skipping {utt_id}: duration {duration:.2f}s exceeds {self.max_duration}s")
-            except Exception as e:
-                print(f"Error loading features for {utt_id}: {e}")
-                continue
-
+                print(f"Skipping {utt_id}: duration {duration:.2f}s exceeds {self.max_audio_duration}s")
         self.data = filtered_data
 
     def __len__(self):
@@ -125,12 +93,10 @@ class IPAPack(ESPnetDataset):
         entry = self.data[utt_id]
         feat_path = os.path.join(self.base_path, entry['feats_path'])
 
-        # Load features with error handling
         try:
             features = kaldiio.load_mat(feat_path)
         except Exception as e:
             print(f"Error loading features for {utt_id}: {e}")
-            # It's better to raise an exception to avoid unexpected None values
             raise e
 
         # Convert text to integer IDs, ignoring spaces
