ee05d6a43385de52ecbb7f11d64a2a0d2f816d4a
kalvinchang <kalvin1204@gmail.com>
2024-11-26 01:26:48 -0500

    fix: clean run.sh

--- egs2/ipapack/asr1/run.sh
+++ egs2/ipapack/asr1/run.sh
@@ -8,24 +8,21 @@ set -o pipefail
 
 train_set="train"
 valid_set="dev"
-test_sets="test"
+test_sets="test_doreco"
 
 encoder=transformer
-frontend=xeus_frozen
-asr_config=conf/tuning/train_asr_${frontend}_${encoder}.yaml
+asr_config=conf/tuning/train_asr_${encoder}.yaml
 inference_config=conf/decode_asr.yaml
 
 ./asr.sh \
-    --stage 11 \
-    --ngpu 4 \
-    --nj 32 \
-    --gpu_inference true \
-    --inference_nj 4 \
+    --token_type word \
+    --max_wav_duration 30 \
+    --use_lm false \
     --token_type word \
     --max_wav_duration 30 \
     --use_lm false \
     --feats_normalize utt_mvn \
-    --feats_type extracted \
+    --feats_type raw \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --inference_asr_model "valid.acc.best.pth" \
