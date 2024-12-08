895a10285ed46ccbcbcc25661e3a8ebd66307671
Kalvin Chang <kalvin1204@gmail.com>
2024-12-08 15:21:12 -0500

    feat: run.sh script to run stage 3

---
+++ egs2/ipapack_plus/asr1/run.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+
+train_set="train"
+valid_set="dev"
+test_sets="test_doreco"
+
+encoder=transformer
+frontend=
+if [[ -n "$frontend" ]]; then
+    asr_config=conf/tuning/train_asr_${frontend}_${encoder}.yaml
+    feats_type=extracted
+else
+    asr_config=conf/tuning/train_asr_${encoder}.yaml
+    feats_type=raw
+    audio_format=flac.ark
+fi
+inference_config=conf/decode_asr.yaml
+
+# 99.5% of data is <= 20 sec long
+max_wav_duration=20
+
+
+# TODO: remove stop_stage
+./asr.sh \
+    --stage 1 \
+    --stop_stage 4 \
+    --ngpu 4 \
+    --nj 32 \
+    --gpu_inference true \
+    --inference_nj 4 \
+    --token_type word \
+    --max_wav_duration $max_wav_duration \
+    --use_lm false \
+    --feats_normalize utt_mvn \
+    --feats_type "${feats_type}" \
+    --audio_format "${audio_format}" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --inference_asr_model "valid.acc.best.pth" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --lm_train_text "data/${train_set}/text" \
+    --bpe_train_text "data/${train_set}/text" \
+    "$@"
