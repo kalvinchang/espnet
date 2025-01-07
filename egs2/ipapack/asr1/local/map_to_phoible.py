d7865437aac728a62424d165405585373f49e2cf
kgnlp <7914956+kgnlp@users.noreply.github.com>
2025-01-07 14:19:44 +0100

    feat: adjusted tokenization strategy in map_to_phoible.py

--- egs2/ipapack/asr1/local/map_to_phoible.py
+++ egs2/ipapack/asr1/local/map_to_phoible.py
@@ -17,6 +17,31 @@ from allophant.phonetic_features import PhoneticAttributeIndexer, FeatureSet
 
 # from allophant import phoneme_segmentation
 from allophant import language_codes
+from marisa_trie import Trie
+
+
+class LongestTokenizer:
+    def __init__(self, dictionary: list[str]) -> None:
+        self._trie = Trie(dictionary)
+
+    def segment(self, string: str, _: bool = False) -> list[str]:
+        trie = self._trie
+        token_stack = [(string, [])]
+        history = []
+
+        while token_stack:
+            suffix, tokens = token_stack.pop()
+            # TODO: Prefixes could be cached for each position in the string
+            history.append((suffix, tokens))
+            for new_token in reversed(trie.prefixes(suffix)):
+                new_suffix = suffix[len(new_token):]
+                new_tokens = [*tokens, new_token]
+                if not new_suffix:
+                    return new_tokens
+                token_stack.append((new_suffix, new_tokens))
+
+        # TODO: Improve error handling
+        raise ValueError(f"Couldn't find a segmentation: {string!r} with last steps: {history[-3:]}")
 
 
 # Mappings for unknown/incomplete glottocodes in DoReCo
@@ -60,7 +85,7 @@ def get_iso6393(
 
 def _normalize_phoneme(phoneme: str) -> str:
     # Handle special case of ç in PHOIBLE not being in NFD
-    return unicodedata.normalize("NFD", phoneme) if "ç" not in phoneme else phoneme
+    return unicodedata.normalize("NFD", phoneme.replace(" ", "").replace(_TIE, "").replace(_LOWER_TIE, "")).replace("ç", "ç")
 
 
 def _is_mark(character: str) -> bool:
@@ -68,11 +93,19 @@ def _is_mark(character: str) -> bool:
     return category.endswith("m") or category == "Sk" or category.startswith("M")
 
 
+def _tokenize_string(string: str, shared_tokenizer: LongestTokenizer | IpaSegmenter, preprocess_string: bool = True) -> List[str]:
+    if preprocess_string:
+        string = _normalize_phoneme(string)
+    return shared_tokenizer.segment(string, True)
+
+
 def extract_vocabulary(
     train_text_path: Path,
     dump_dir: Path,
     phoneme_indexer: PhoneticAttributeIndexer,
     glottomap: Dict[str, Dict[str, str]],
+    phonepiece_pretokenized: bool = False,
+    allow_partial_segmentation: bool = False,
 ) -> Dict[str, List[str]]:
     delimiters = re.compile("[ ˈ]")
     inventory = set()
@@ -80,12 +113,12 @@ def extract_vocabulary(
     glottomap_codes = glottomap["code"]
     glottomap_closest = glottomap["closest"]
 
-    phoneme_list = phoneme_indexer.full_attributes.phonemes.tolist()
+    phoneme_list = phoneme_indexer.full_subset_attributes.phonemes.tolist()
     language_mappings = {}
     supported_phonemes = set(phoneme_list)
     missing_phonemes = set()
-    tokenizer = IpaSegmenter(phoneme_list)
-    phonepiece = read_ipa()
+    shared_tokenizer = IpaSegmenter(phoneme_list) if allow_partial_segmentation else LongestTokenizer(phoneme_list)
+    phonepiece = read_ipa() if phonepiece_pretokenized else None
 
     with (train_text_path).open("r", encoding="utf-8") as file:
         last_language = ""
@@ -122,29 +155,35 @@ def extract_vocabulary(
                 if inventory is None:
                     inventories[language] = inventory = set()
 
-            phonemes = [
-                tokenizer.segment(
-                    phoneme.replace(_TIE, "").replace(_LOWER_TIE, ""), True
-                )
-                for word in delimiters.split(transcription)
-                for phoneme in phonepiece.tokenize(word)
-            ]
-            for phoneme in phonemes:
-                subsegment = _normalize_phoneme(phoneme[0])
-                if len(phoneme) != 1 or subsegment not in supported_phonemes:
-                    # Handle rare cases in IPAPack where diacritic ordering
-                    # doesn't correspond to the canonical ordering in PHOIBLE
-                    reordered = subsegment[:-2] + subsegment[-2::-1]
-                    if (
-                        all(map(_is_mark, subsegment[-2::-1]))
-                        and reordered not in supported_phonemes
-                    ):
-                        unsplit = "".join(map(_normalize_phoneme, phoneme))
-                        missing_phonemes.add((tuple(phoneme), unsplit))
-                        # inventory.add(unsplit)
-                        continue
+            if phonepiece is None:
+                # TODO: Temporary workaround for inconsistent diacritic ordering interfering with tokenization
+                phonemes = _tokenize_string(transcription.replace("ːˠ", "ˠː").replace("ːʲ", "ʲː").replace("ʰʷ", "ʷʰ").replace("ʰʲ", "ʲʰ"), shared_tokenizer)
+            else:
+                phonemes = [
+                    _tokenize_string(phoneme, shared_tokenizer)
+                    for word in delimiters.split(transcription)
+                    for phoneme in phonepiece.tokenize(word)
+                ]
 
-                    subsegment = reordered
+            for phoneme in phonemes:
+                if isinstance(phoneme, str):
+                    subsegment = phoneme
+                else:
+                    subsegment = _normalize_phoneme(phoneme[0])
+                    if len(phoneme) != 1 or subsegment not in supported_phonemes:
+                        # Handle rare cases in IPAPack where diacritic ordering
+                        # doesn't correspond to the canonical ordering in PHOIBLE
+                        reordered = subsegment[:-2] + subsegment[-2::-1]
+                        if (
+                            all(map(_is_mark, subsegment[-2::-1]))
+                            and reordered not in supported_phonemes
+                        ):
+                            unsplit = "".join(map(_normalize_phoneme, phoneme))
+                            missing_phonemes.add((tuple(phoneme), unsplit))
+                            # inventory.add(unsplit)
+                            continue
+
+                        subsegment = reordered
 
                 inventory.add(subsegment)
 
@@ -170,6 +209,7 @@ def collect_inventories(
     dump_dir: Path,
     data_dir: Path,
     glottolog_languages: Path,
+    phonepiece_pretokenized: bool = False,
     skip_vocab_generation: bool = False,
 ) -> None:
     with open("local/allophoible/allophoible_v2.csv", "r", encoding="utf-8") as file:
@@ -193,7 +233,11 @@ def collect_inventories(
             inventory_lists = json.load(file)
     else:
         inventory_lists = extract_vocabulary(
-            train_text_path, dump_dir, phoneme_indexer, glottomap
+            train_text_path,
+            dump_dir,
+            phoneme_indexer,
+            glottomap,
+            phonepiece_pretokenized,
         )
 
     if missing_phonemes := inventory_lists.get("MISSING"):
@@ -205,7 +249,9 @@ def collect_inventories(
     token_path = words_dir / "tokens.txt"
 
     # Backup original token inventory
-    shutil.copy2(token_path, words_dir / "tokens.txt.original")
+    token_backup_path = words_dir / "tokens.txt.original"
+    if not token_backup_path.exists():
+        shutil.copy2(token_path, token_backup_path)
 
     with token_path.open("r", encoding="utf-8") as file:
         clusters = []
@@ -262,28 +308,32 @@ def collect_inventories(
     with (dump_dir / "mappings.json").open("w", encoding="utf-8") as file:
         json.dump(phoneme_mappings, file)
 
-    with token_path.open("w", encoding="utf-8") as file:
-        shared_mapped_inventory = {
-            phoneme
-            for target_phonemes in phoneme_mappings.values()
-            for phoneme in target_phonemes
-        }
-        # Replace the original with the mapped shared phoneme inventory
-        # while preserving special token positions at the start and end
-        file.writelines(
-            token + "\n"
-            for token in itertools.chain(
-                start_special_tokens, shared_mapped_inventory, end_special_tokens
-            )
-        )
+    # with token_path.open("w", encoding="utf-8") as file:
+    #     shared_mapped_inventory = {
+    #         phoneme
+    #         for target_phonemes in phoneme_mappings.values()
+    #         for phoneme in target_phonemes
+    #     }
+    #     # Replace the original with the mapped shared phoneme inventory
+    #     # while preserving special token positions at the start and end
+    #     file.writelines(
+    #         token + "\n"
+    #         for token in itertools.chain(
+    #             start_special_tokens, shared_mapped_inventory, end_special_tokens
+    #         )
+    #     )
 
     print(f"Wrote new shared inventory to {token_path}")
-    return
 
     # Backup original transcriptions
-    shutil.copy2(train_text_path, dump_dir / "train/text.original")
+    train_backup_path = dump_dir / "train/text.original"
+    if not train_backup_path.exists():
+        shutil.copy2(train_text_path, train_backup_path)
+
     dev_text_path = dump_dir / "dev/text"
-    shutil.copy2(dev_text_path, dump_dir / "dev/text.original")
+    dev_backup_path = dump_dir / "dev/text.original"
+    if not dev_backup_path.exists():
+        shutil.copy2(dev_text_path, dev_backup_path)
 
     last_language = ""
     language = ""
@@ -303,15 +353,16 @@ def collect_inventories(
 
             if language_code != last_language:
                 last_language = language_code
-                language = get_iso6393(language_code, dataset, glottomap)
+                language = get_iso6393(language_code, dataset, glottomap["code"], glottomap["closest"])
                 mapping = {} if language is None else phoneme_mappings[language]
 
             remapped = [
                 remapped
                 for phoneme in transcription
-                for remapped in mapping.get(phoneme, phoneme)
+                # TODO: Improve to avoid double normalization
+                for remapped in mapping.get(_normalize_phoneme(phoneme), _normalize_phoneme(phoneme))
             ]
-            # print(len(transcription), len(remapped), transcription, remapped)
+            print(len(transcription), len(remapped), transcription, remapped)
 
 
 def main(args: Sequence[str]) -> None:
@@ -335,6 +386,11 @@ def main(args: Sequence[str]) -> None:
         help="path to a glottolog languages.csv file for handling glottocodes in DORECO",
     )
     parser.add_argument(
+        "--phonepiece_pretokenized",
+        action="store_true",
+        help="Pretokenize with phonepiece - e.g. to identify missing phonemes in the allophoible tokenizer",
+    )
+    parser.add_argument(
         "--skip_vocab_generation",
         action="store_true",
         help="Skips directly to phoneme remapping using previously extracted phoneme vocabularies",
@@ -345,6 +401,7 @@ def main(args: Sequence[str]) -> None:
         arguments.dump_dir,
         arguments.data_dir,
         arguments.glottolog_languages,
+        arguments.phonepiece_pretokenized,
         arguments.skip_vocab_generation,
     )
 
