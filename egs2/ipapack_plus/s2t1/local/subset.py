af960dcf07f0489f3946da8fedd34aa2ac14eaea
Kalvin Chang <kalvin1204@gmail.com>
2025-01-21 14:35:46 -0500

    fix: revise local/subset.py to handle the new prev/ctc files (to guarantee unique utt id)

--- egs2/ipapack_plus/s2t1/local/subset.py
+++ egs2/ipapack_plus/s2t1/local/subset.py
@@ -22,7 +22,7 @@ def subsample(olddir, newdir, dataset, ratio, suffix=""):
     print(f"-> Subsample {dataset} by 1/{ratio}...")
     currentdir = f"{newdir}/{dataset}{suffix}"
     os.system(f"mkdir -p {currentdir}/texts")
-    
+
     # 1. direct copy: feats_type & audio_format
     os.system(f"cp {olddir}/{dataset}/feats_type {currentdir}/feats_type")
     os.system(f"echo flac.ark > {currentdir}/audio_format")
@@ -33,20 +33,17 @@ def subsample(olddir, newdir, dataset, ratio, suffix=""):
         for i in range(4): # repeat 4 times for 4 tasks
             os.system(f"cat {currentdir}/{file} >> {currentdir}/{file}.tmp")
         os.system(f"mv {currentdir}/{file}.tmp {currentdir}/{file}")
-    for file in ["text.asr", "text.ctc", "text.g2p_prev", "text.p2g_prev", "text", "text.asr_ctc", "text.g2p", "text.p2g", "text.prev"]:
+
+
+    for file in ["text", "text.prev", "text.ctc", "text.asr", "text.asr_prev", "text.asr_ctc", "text.g2p", "text.g2p_prev", "text.g2p_ctc", "text.p2g", "text.p2g_prev", "text.p2g_ctc"]:
         os.system(f"awk 'NR % {ratio} == 1' {olddir}/{dataset}/{file} > {currentdir}/texts/{file}")
-    os.system(f"cp {currentdir}/texts/text {currentdir}/texts/text.na")
-    # remove the text and replace with <na> since no previous text for ASR, phoneme recognition
-    os.system(f"sed -i 's/ .*/ <na>/' {currentdir}/texts/text.na")
-    
+
     # 3. combine text files
     for file in ["text", "text.asr", "text.g2p", "text.p2g"]:
         os.system(f"cat {currentdir}/texts/{file} >> {currentdir}/text")
-    for file in ["text.na", "text.na", "text.g2p_prev", "text.p2g_prev"]:
+    for file in ["text.prev", "text.asr_prev", "text.g2p_prev", "text.p2g_prev"]:
         os.system(f"cat {currentdir}/texts/{file} >> {currentdir}/text.prev")
-    # note: for G2P, the ctc text comes from phoneme recognition
-    #       for P2G, the ctc text comes from ASR
-    for file in ["text.ctc", "text.asr_ctc", "text.ctc", "text.asr_ctc"]:
+    for file in ["text.ctc", "text.asr_ctc", "text.g2p_ctc", "text.p2g_ctc"]:
         os.system(f"cat {currentdir}/texts/{file} >> {currentdir}/text.ctc")
 
     print("Finished!")
@@ -83,7 +80,7 @@ def genwav(olddir, currentdir, nsplit=32):
         os.system(f"mkdir {formatdir}")
         os.system(f"mv {currentdir}/data/wav.{i:02d} {formatdir}/wav.scp")
         os.system(f"mv {currentdir}/data/samples.{i:02d} {formatdir}/utt2num_samples")
-    
+
     # 2. In each split, get new data_wav.ark and wav.scp
     for i in tqdm(range(nsplit)):
         formatdir = f"{currentdir}/data/format.{i+1}"
@@ -92,7 +89,7 @@ def genwav(olddir, currentdir, nsplit=32):
         d = kaldiio.load_scp(f'{formatdir}/wav.scp')
         kaldiio.save_ark(f'{formatdir}/data_wav.ark', d, 
             write_function="soundfile", scp=f'{formatdir}/wav.scp')
-        
+
     # 3. Update wav.scp by combining wav.scp in data/format.1~nsplit
     os.system(f"rm {currentdir}/wav.scp")
     for i in range(nsplit):
