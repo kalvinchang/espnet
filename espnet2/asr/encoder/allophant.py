fix: Fixed length calculation in huggingface frontend becoming negative for very short utterances

espnet2/asr/encoder/allophant.py
@@ -520,9 +520,7 @@ class AllophantLayers(AbsEncoder):
                 dtype=torch.int64,
             )
             # Assume that the allophone layer should not be used when a custom phoneme inventory is provided
-            output = self._allophone_layer(
-                output, language_ids, target_feature_indices is not None
-            )
+            output = self._allophone_layer(output, language_ids)
 
             # Return l2_penalty during training to keep the allophone matrix
             # close to its initialization following Li et al., 2020

espnet2/asr/frontend/huggingface.py
@@ -69,7 +69,8 @@ class HuggingFaceFrontend(AbsFrontend):
         for kernel_size, stride in zip(config.conv_kernel, config.conv_stride):
             lengths = torch.div(lengths - kernel_size, stride, rounding_mode="floor") + 1
 
-        return lengths
+        # Ensure lengths are >= 0 for very short utterances
+        return torch.maximum(lengths, torch.tensor(0))
 
     def forward(
         self, inputs: torch.Tensor, input_lengths: torch.Tensor